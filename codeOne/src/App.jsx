b7a8494a22ba4cda9fa7f3bd062549d649f66fb1
adrikaDwivedi <adrikad97@gmail.com>
2026-04-30 13:30:13 +0530

    changes

--- codeOne/src/App.jsx
+++ codeOne/src/App.jsx
@@ -1,10 +1,20 @@
+import React from 'react'
+import logo from './assets/logo.png'
+import './App.css'
 
 function App() {
-
   return (
-    <>
-    <h1>Hello world</h1>
-    </>
+    <div className="container" >
+    <img className="logo" src={logo} alt="Logo" />
+    <h1 className="heading">Fun facts about react</h1>
+    <ul className="listItems">
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walker</li>
+      <li>Has well over 200k stars on Github</li>
+      <li>Is maintained by Meta</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+    </div>
   )
 }
 
