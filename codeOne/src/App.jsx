9b7b15e76f3f82761082c8344e22eadf382e3534
AdrikaD <adrika@Adrikas-MacBook-Air.local>
2026-05-10 19:46:47 +0530

    changes

--- codeOne/src/App.jsx
+++ codeOne/src/App.jsx
@@ -1,19 +1,11 @@
 import React from 'react'
-import logo from './assets/logo.png'
 import './App.css'
+import CodeOne from './components/codeOne'
 
 function App() {
   return (
-    <div className="container" >
-    <img className="logo" src={logo} alt="Logo" />
-    <h1 className="heading">Fun facts about react</h1>
-    <ul className="listItems">
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walker</li>
-      <li>Has well over 200k stars on Github</li>
-      <li>Is maintained by Meta</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
+    <div>
+      <CodeOne />
     </div>
   )
 }
