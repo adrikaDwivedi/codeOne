fa562563fcb35bea7f83a5fabff9c0de24503749
adrikaD <adrikad97@gmail.com>
2026-05-17 22:26:44 +0530

    codeTwo

---
+++ codeTwo/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return (
+        <header className="header-main">
+            <div className="img-heading">
+                <img src  alt="globe image"/>
+                <h1>my travel journal. </h1>
+            </div>
+        </header>
+    )
+}
